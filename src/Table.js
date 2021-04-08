0e5ed8c1374bd4dcfaef4527deaa6d17ea89e9ed
Web-Dev-Jr <samkautz@yahoo.com>
2021-04-07 21:45:12 -0500

    Update Render in Table.js

--- src/Table.js
+++ src/Table.js
@@ -23,7 +23,22 @@ export default class Table extends React.Component {
   // } 
   
   render() {
-    console.log(this.state)
+    if(this.props.appState.myData.length <1 ) {
+      console.log(this.state)
+      axios.get('https://swapi.dev/api/people')
+      .then(response => {
+      const myData = response.data.results;
+      this.setState({ myData });
+      })
+    } else if(this.props.appState.myData.length > 0) {
+      // console.log(this.props.appState.myData[0].api)
+      const userSearch = this.props.appState.myData[0].api
+      axios.get(userSearch)
+      .then(response => {
+      const myData = response.data.results;
+      this.setState({ myData });
+      })
+    }
     const returnData = this.state.myData.map(returnItems => {
       let {name, birth_year, homeworld, height, mass, species} = returnItems;
       if(homeworld === "http://swapi.dev/api/planets/1/") {
@@ -38,7 +53,6 @@ export default class Table extends React.Component {
     
     if (species = "https://swapi.dev/api/species/2/") {
       species = "Droid";
-      console.log(this.state)
     }
       return(
         <tr>
@@ -58,7 +72,7 @@ return (
         <th scope="col">Name</th>
         <th scope="col">DOB</th>
         <th scope="col">Planet</th>
-        <th scope="col">Height</th>
+        <th scope="col">Height</th> 
         <th scope="col">Mass</th>
         <th scope="col">Species</th>
       </tr>
