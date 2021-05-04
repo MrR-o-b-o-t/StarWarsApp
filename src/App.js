059552d0286ed6a5abbdda7feaef87c8cfc39263
Web-Dev-Jr <samkautz@yahoo.com>
2021-05-03 19:02:55 -0500

    Refine and commit axios get request to pull all pages of data available. Minor refinements to pagination styling.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import paginationFactory from 'react-bootstrap-table2-paginator'
 import * as ReactBootStrap from "react-bootstrap"
 import BootstrapTable from "react-bootstrap-table-next"
 import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css'
+
 const columns = [
   {dataField: "name", text: "Name"},
   {dataField: "birth_year", text: "D.O.B"},
@@ -18,28 +19,24 @@ const columns = [
   {dataField: "species", text: "Species"}
 ]
 const options = {
-  paginationSize: 3,
+  paginationSize: 2,
   pageStartIndex: 1,
   // alwaysShowAllBtns: true,
-  // withFirstAndLast: false,
-  // hideSizePerPage: true, 
+  withFirstAndLast: false,
+  hideSizePerPage: true, 
   hidePageListOnlyOnePage: true, 
-  firstPageText: 'First',
-  prePageText: 'Back',
-  nextPageText: 'Next',
-  lastPageText: 'Last',
+  // firstPageText: 'First',
+  // prePageText: 'Back',
+  // nextPageText: 'Next',
+  // lastPageText: 'Last',
   nextPageTitle: 'First page',
   prePageTitle: 'Pre page',
   firstPageTitle: 'Next page',
   lastPageTitle: 'Last page',
-  showTotal: true,
+  showTotal: false,
   disablePageTitle: true,
   sizePerPageList: [{
-    text: '5', value: 5
-  }, {
     text: '10', value: 10
-  }, {
-    text: '20', value: 20
   }] 
 };
 
@@ -50,45 +47,62 @@ export default class App extends Component {
   };
 
   componentDidMount() {
-    const getData = async () => {
-      try {
-        const data = await axios.get("https://swapi.dev/api/people/")
-        console.log(data.data.results)
-        const myData = data.data.results
-        console.log(myData)
-        this.setState({ myData })
-        console.log(this.state)
-      } catch (err) {
-        console.log(err)
-      }
-    };
-    getData();
+    function getAllStarwarsPeople() {
+      let people = [];
+      return axios("https://swapi.dev/api/people/")
+        .then((response) => {
+          people = response.data.results;
+          return response.data.count;
+        })
+        .then((count) => {
+          const numberOfPagesLeft = Math.ceil((count - 1) / 10);
+          let promises = [];
+          for (let i = 2; i <= numberOfPagesLeft; i++) {
+            promises.push(axios(`https://swapi.dev/api/people?page=${i}`));
+          }
+          return Promise.all(promises);
+        })
+        .then((response) => {
+          people = response.reduce(
+            (acc, data) => [...acc, ...data.data.results],
+            people
+          );
+          return people;
+        })
+        .catch((err) => console.log(err));
+    }
+    (async () => {
+      const myData = await getAllStarwarsPeople();
+      this.setState({myData})
+      console.log(this.state)
+    })();
+
   }
 
   componentDidUpdate() {
     if (this.state.isTrue[0] === true) {
-      try{
-        const userSearch = this.state.myData[0].api;
+      try {
+        const userSearch = this.state.myData[0].api
         axios.get(userSearch).then((response) => {
-        const myData = response.data.results
-        this.setState({ myData })
-        this.state.isTrue = false
-      })
-    } catch (err) {
-      console.log(err)
-    }
+          const myData = response.data.results
+          this.setState({ myData })
+          this.state.isTrue = false
+        })
+      } catch (err) {
+        console.log(err)
+      }
     }
   }
 
   addData = async (moreData) => {
     try {
-      const searchReturn = await axios.get(moreData.api)
-      const myData = searchReturn.data.results
+      const searchReturn = await axios.get(moreData.api);
+      const myData = searchReturn.data.results;
       this.setState({
         myData,
-      })
+      });
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
   };
 
